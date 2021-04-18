3288eaf5d7a964f7d2860db6d9ec3c2e8b2066b0
jgr <jonathan.gruner@tset.com>
2021-04-19 01:43:43 +0200

    threads detached

--- src/include/concurrent_monte_carlo.h
+++ src/include/concurrent_monte_carlo.h
@@ -15,51 +15,55 @@ namespace muskat {
 
 
 inline void execute_worker(
-	const PossibleWorlds &possible_worlds, //TODO: Not a reference, so not every thread has to access the same memory there?
-	std::vector<std::array<uint8_t, 32>> &result, //We need this to be not destructed until the thread is joined.
-	stdc::SpinLock &result_write_lock, //Reading is always allowed, since noone else writes.
-	const std::atomic<bool> &should_continue,
+	PossibleWorlds possible_worlds, //TODO: Not a reference, so not every thread has to access the same memory there?
+	const std::unique_ptr<std::vector<std::array<uint8_t, 32>>> result_ptr,
+	const std::unique_ptr<stdc::SpinLock> result_write_lock_ptr, //Reading is always allowed, since noone else writes.
+	const std::unique_ptr<std::atomic<bool>> should_continue_ptr,
 	size_t worker_id
 ) {
-	assert(result.size() == 0);
+	assert(result_ptr->size() == 0);
 
 	auto rng = stdc::seeded_RNG(stdc::DeterministicSourceOfRandomness{0, static_cast<unsigned int>(worker_id)});
 
 	//Stop when the main thread signals that time is up via should_continue.
 	//It's not enough to just wait until the main thread locks the lock, because we coudn't be sure that we
 	//are always faster than the main thread by locking it, therefore blocking it forever.
-	while (should_continue.load(std::memory_order::relaxed)) {
+	while (should_continue_ptr->load(std::memory_order::relaxed)) {
 		auto situation = possible_worlds.get_one_uniformly_clever(rng);
 		auto solver = muskat::SituationSolver{possible_worlds.get_game_type()};
 
 		solver.calculate_potential_score_2(situation);
 		auto points = solver.score_for_possible_plays(situation);
 
-		auto need_to_reallocate = result.size() == result.capacity();
+		auto need_to_reallocate = result_ptr->size() == result_ptr->capacity();
 
 		if (!need_to_reallocate) {
-			if (!result_write_lock.try_lock()) {
+			if (!result_write_lock_ptr->try_lock()) {
 				//TODO: Is this guaranteed to be observed in this order when the main thread does it in this order?
-				assert(!should_continue.load(std::memory_order::relaxed));
-				return;
+				assert(!should_continue_ptr->load(std::memory_order::relaxed));
+				break;
 			}
-			result.push_back(points);
-			result_write_lock.unlock();
+			result_ptr->push_back(points);
+			result_write_lock_ptr->unlock();
 			continue;
 		}	
 
 		auto result_copy = std::vector<std::array<uint8_t, 32>>{};
-		result_copy.reserve(2 * (result.capacity() + 1));
-		result_copy = result;
+		result_copy.reserve(2 * (result_ptr->capacity() + 1));
+		result_copy = *result_ptr;
 		result_copy.push_back(points);
 		
-		if (!result_write_lock.try_lock()) {
+		if (!result_write_lock_ptr->try_lock()) {
 			//TODO: Is this guaranteed to be observed in this order when the main thread does it in this order?
-			assert(!should_continue.load(std::memory_order::relaxed));
-			return;
+			assert(!should_continue_ptr->load(std::memory_order::relaxed));
+			break;
 		}
-		result = std::move(result_copy);
-		result_write_lock.unlock();
+		*result_ptr = std::move(result_copy);
+		result_write_lock_ptr->unlock();
+	}
+
+	while (should_continue_ptr->load(std::memory_order::relaxed)) {
+		//Nothing.
 	}
 }
 
@@ -74,24 +78,38 @@ inline void execute_worker(
 
 	std::cout << "Number of threads: " << number_of_threads << ".\n";
 
-	auto threads_should_continue = std::atomic{true};
 
 	//This way, threads are joined before the results are destructed, guranteeing that the results exists while threads exist.
-	auto results_th = std::vector<std::vector<std::array<uint8_t, 32>>>(number_of_threads);
+	auto results_temp = std::vector<std::unique_ptr<std::vector<std::array<uint8_t, 32>>>>(number_of_threads);
 	//Same for the locks
-	auto locks_th = std::vector<stdc::SpinLock>(number_of_threads);
-	auto threads = std::vector<std::jthread>{};
-	threads.reserve(number_of_threads);
+	auto locks_temp = std::vector<std::unique_ptr<stdc::SpinLock>>(number_of_threads);
+	auto should_continue_temp = std::vector<std::unique_ptr<std::atomic<bool>>>(number_of_threads);
 
+	auto results_th = std::vector<std::vector<std::array<uint8_t, 32>> *>(number_of_threads);
+	auto locks_th = std::vector<stdc::SpinLock *>(number_of_threads);
+	auto should_continue_th = std::vector<std::atomic<bool> *>(number_of_threads);
+	
+	
+
+	for (auto i = 0_z; i < number_of_threads; ++i) {
+		results_temp[i] = std::make_unique<std::vector<std::array<uint8_t, 32>>>();
+		locks_temp[i] = std::make_unique<stdc::SpinLock>();
+		should_continue_temp[i] = std::make_unique<std::atomic<bool>>(true);
+
+		results_th[i] = results_temp[i].get();
+		locks_th[i] = locks_temp[i].get();
+		should_continue_th[i] = should_continue_temp[i].get();
+	}
+	
 	for (auto thread_id = 0_z; thread_id < number_of_threads; ++thread_id) {
-		threads.push_back(std::jthread{
+		std::jthread{
 			execute_worker,
-			std::cref(possible_worlds),
-			std::ref(results_th[thread_id]),
-			std::ref(locks_th[thread_id]),
-			std::cref(threads_should_continue),
+			possible_worlds,
+			std::move(results_temp[thread_id]),
+			std::move(locks_temp[thread_id]),
+			std::move(should_continue_temp[thread_id]),
 			thread_id
-		});
+		}.detach();
 	}
 
 	std::this_thread::sleep_for(std::chrono::seconds(10));
@@ -101,7 +119,6 @@ inline void execute_worker(
 
 	auto results = std::vector<std::array<uint8_t, 32>>{};
 
-	threads_should_continue = false;
 	while (results_obtained != number_of_threads) {
 		for (auto thread_id = 0_z; thread_id < number_of_threads; ++thread_id) {
 			if (results_obtained_th[thread_id]) {
@@ -109,11 +126,13 @@ inline void execute_worker(
 			}
 
 			//Lock forever.
-			if (!locks_th[thread_id].try_lock()) {
+			if (!locks_th[thread_id]->try_lock()) {
 				continue;
 			}
 
-			std::move(RANGE(results_th[thread_id]), std::back_inserter(results));
+			std::move((results_th[thread_id])->begin(), (results_th[thread_id])->end(), std::back_inserter(results));
+			*should_continue_th[thread_id] = false;
+			
 			results_obtained_th[thread_id] = true;
 			++results_obtained;
 		}
