1a7f124ff856b06d81136c65c0f5ca8d0c096fc2
jgr <jonathan.gruner@tset.com>
2021-05-11 22:49:40 +0200

    first version of the play order heuristic

--- src/include/situation_solver.h
+++ src/include/situation_solver.h
@@ -9,6 +9,74 @@
 namespace muskat {
 
 
+	[[nodiscard]] inline auto get_cards_to_consider(
+		Situation sit,
+		GameType game,
+		MaybeCard maybe_preference
+	) {
+		using namespace stdc::literals;
+
+		auto result = std::array<MaybeCard, 10>{};
+		assert(result[0] == MaybeCard{});
+		assert(result[1] == MaybeCard{});
+		assert(result[9] == MaybeCard{});
+		
+		auto cards = next_possible_plays(sit, game);
+		assert(!cards.empty());
+		assert(cards.size() < 11);
+
+		auto next_index = 0_z;
+		if (maybe_preference) {
+			result[next_index] = *maybe_preference;
+			cards.remove(*maybe_preference);
+			++next_index;
+		}
+
+		auto maybe_forced_tt = [&]() -> std::optional<TrickAndGameType> {
+			if (auto maybe_first = sit.get_maybe_first_trick_card()) {
+				return TrickAndGameType{*maybe_first, game};
+			}
+			return std::nullopt;
+		}();
+
+		auto n = next(sit.active_role());
+		auto nn = next(n);
+		auto n_h = sit.hand(n);
+		auto nn_h = sit.hand(nn);
+		auto get_options = [&](auto card) {
+			if (maybe_forced_tt) {
+				return 0ul;
+			}
+			auto tt = TrickAndGameType{card, game};
+			return legal_response_cards(n_h, tt).size() * legal_response_cards(nn_h, tt).size();
+		};
+
+
+
+		while(!cards.empty()) {
+			auto cards_mod = cards;
+			auto best_card = cards_mod.remove_next();
+			auto best_options = get_options(best_card);
+			auto best_power = to_power(best_card, maybe_forced_tt.value_or(TrickAndGameType{best_card, game}));
+			while (!cards_mod.empty()) {
+				auto card = cards_mod.remove_next();
+				auto options = get_options(card);
+				auto power = to_power(card, maybe_forced_tt.value_or(TrickAndGameType{card, game}));
+				if (std::tuple{options, -power} < std::tuple{best_options, -best_power}) {
+					best_card = card;
+					best_options = options;
+					best_power = power;
+				}
+			}
+
+			result[next_index] = best_card;
+			cards.remove(best_card);
+			++next_index;
+		}
+
+		return result;
+	}
+
 	class Bounds {
 	public:
 		Points m_lower;
@@ -48,12 +116,15 @@ namespace muskat {
 	[[nodiscard]] inline auto quick_bounds(Situation sit, GameType game) {
 		constexpr auto max_points = Points{120};
 		auto upper = static_cast<Points>(max_points - to_points(sit.cellar(), game));
-		return Bounds{0, upper};
+		return std::pair{Bounds{0, upper}, MaybeCard{}};
 	}
 
 	class SituationSolver {
 	private:
-		using LookUpTable = robin_hood::unordered_flat_map<Situation, Bounds>;
+		using LookUpTable = robin_hood::unordered_flat_map<
+			Situation,
+			std::pair<Bounds, MaybeCard>
+		>;
 
 		LookUpTable m_look_up;
 		GameType m_game;
@@ -79,7 +150,10 @@ namespace muskat {
 	
 	public:
 		SituationSolver(GameType game) : m_look_up{}, m_game{game} {
-			[[maybe_unused]] auto strict_bounds_childs = Bounds{};
+			[[maybe_unused]] auto strict_bounds_childs = std::pair{
+				Bounds{},
+				MaybeCard{}
+			};
 			m_look_up[Situation{Role::Declarer}] = strict_bounds_childs;
 			m_look_up[Situation{Role::FirstDefender}] = strict_bounds_childs;
 			m_look_up[Situation{Role::SecondDefender}] = strict_bounds_childs;
@@ -94,10 +168,16 @@ namespace muskat {
 	private:
 		//This is the highly optimized one.
 		template<bool is_declarer>
-		[[nodiscard]] auto improve_bounds_to_decide_threshold(Bounds bounds, Situation sit, Points threshold) {
+		[[nodiscard]] auto improve_bounds_to_decide_threshold(
+			std::pair<Bounds, MaybeCard> bounds_pref,
+			Situation sit,
+			Points threshold
+		) -> std::pair<Bounds, MaybeCard> {
 			using namespace stdc::literals;
 			
 			assert((sit.active_role() == Role::Declarer) == is_declarer);
+
+			auto &[bounds, pref] = bounds_pref;
 
 			assert(!decides_threshold(bounds, threshold));
 
@@ -107,9 +187,17 @@ namespace muskat {
 			//We catch these via strict bounds.
 			assert(!remaining_possible_plays.empty());
 
-			while (!remaining_possible_plays.empty()) {
-				auto card = remaining_possible_plays.remove_next();
-				
+			auto cards_to_consider = get_cards_to_consider(sit, m_game, pref);
+
+			auto maybe_deciding_card = MaybeCard{};
+
+			for (auto mcard : cards_to_consider) {
+				if (!mcard) {
+					break;
+				}
+
+				auto card = *mcard;
+
 				auto child = sit;
 				auto points = child.play_card(card, m_game);
 				auto threshold_child = threshold <= points ? Points{} : static_cast<Points>(threshold - points);
@@ -125,11 +213,12 @@ namespace muskat {
 				}
 				
 				if (decides_threshold(bounds, threshold)) {
+					maybe_deciding_card = card;
 					break;
 				}
 			}
 			
-			if (remaining_possible_plays.empty()) {
+			if (!maybe_deciding_card) {
 				if constexpr (is_declarer) {
 					//No winning child.
 					bounds.update_upper(bound_calculated_over_all_children);
@@ -139,23 +228,25 @@ namespace muskat {
 				}
 			}
 
-			return bounds;
+			return {bounds, maybe_deciding_card};
 		}
 
 	public:
 		[[nodiscard]] auto bounds_deciding_threshold(Situation sit, Points threshold) -> Bounds {
 			
-			auto bounds = current_bounds(sit);
-			if (!decides_threshold(bounds, threshold)) {
+			auto bounds_pref = current_bounds(sit);
+			if (!decides_threshold(bounds_pref.first, threshold)) {
 				if (sit.active_role() == Role::Declarer) {
-					bounds = improve_bounds_to_decide_threshold<true>(bounds, sit, threshold);
+					bounds_pref = improve_bounds_to_decide_threshold<true>(bounds_pref, sit, threshold);
 				} else {
-					bounds = improve_bounds_to_decide_threshold<false>(bounds, sit, threshold);
+					bounds_pref = improve_bounds_to_decide_threshold<false>(bounds_pref, sit, threshold);
 				}
-				assert(decides_threshold(bounds, threshold));
-				current_bounds(sit) = bounds;
+				assert(decides_threshold(bounds_pref.first, threshold));
+				
+				//TODO: Surely existing, no?
+				current_bounds(sit) = bounds_pref;
 			}
-			return bounds;
+			return bounds_pref.first;
 		}
 
 		[[nodiscard]] auto still_makes_at_least(Situation sit, Points expected_points) {
